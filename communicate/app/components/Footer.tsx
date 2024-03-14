1e112aa3e34d5b2d094d7f2cbbe08d29fb08a39a
arminsandhu <113546432+arminsandhu@users.noreply.github.com>
2024-03-14 00:28:52 -0600

    added footer + line component and react icons

---
+++ communicate/app/components/Footer.tsx
@@ -0,0 +1,22 @@
+'use client'
+import { SocialIcon } from 'react-social-icons'
+import Line from './line'
+
+const Footer = () => {
+  return (
+    <div>
+        <Line />        
+        <div className='footer grid grid-col-2 bg-forest-green text-black flex justify-center content-center h-48 p-2'>
+        <div className='flex justify-center gap-4 p-4'>
+            <SocialIcon bgColor='currentColor' fgColor='transparent' url='https://instagram.com' />
+            <SocialIcon bgColor='currentColor' fgColor='transparent' url='https://linkedin.com' />
+            <SocialIcon bgColor='currentColor' fgColor='transparent' url='https://tiktok.com' />
+        </div>
+        <div>
+            <h1 className='text-center'>Copyright © CommUNIcate</h1>
+        </div>
+        </div>
+    </div>
+  )
+}
+export default Footer
---
+++ communicate/app/components/line.tsx
@@ -0,0 +1,11 @@
+const line = () => {
+  return (
+    <div>
+      <div className="px-40 bg-white">
+        <hr className="w-full my-1 border-t border-neutral-100 opacity-100 dark:opacity-50" />
+      </div>
+    </div>
+  );
+};
+
+export default line;
