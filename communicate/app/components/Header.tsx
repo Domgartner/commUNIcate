31755c5521b3897ee0096692f6a80f63bed9c2a8
arminsandhu <113546432+arminsandhu@users.noreply.github.com>
2024-03-19 10:03:02 -0600

    header fix

--- communicate/app/components/Header.tsx
+++ communicate/app/components/Header.tsx
@@ -1,16 +1,18 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser } from "@fortawesome/free-solid-svg-icons";
 import Line from './line'
+import { useRouter } from 'next/navigation'
 
 export default function Header() {
+    const router = useRouter()
 
     return (
             <div className="flex flex-col">
                 <div className="flex flex-row py-2 px-10 justify-between">
                     <h1 className="text-4xl py-2">Comm<span className="text-olive">UNI</span>cate</h1>
-                    <div className='text-4xl pt-2'>
+                    <button className='text-4xl pt-2' onClick={() => router.push('/profile')}>
                         <FontAwesomeIcon icon={faUser} />
-                    </div>
+                    </button>
                 </div>
                 <Line />
             </div>
