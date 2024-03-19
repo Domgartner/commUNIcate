e9816298a8fb735065683f252c9852dbeeea8680
arminsandhu <113546432+arminsandhu@users.noreply.github.com>
2024-03-19 10:06:09 -0600

    removing unnessicary info from lambda functions

--- functions/get_events/main.py
+++ functions/get_events/main.py
@@ -1,2 +1,24 @@
+# add your get-obituaries function here
+import json
+import boto3
+from boto3.dynamodb.conditions import Key
+
+dynamodb = boto3.resource("dynamodb")
+table = dynamodb.Table("")
+
 def handler(event, context):
-    pass
+
+
+    try:
+        res = table.scan()
+        return {
+            "statusCode": 200,
+            "body": json.dumps(res["Items"])
+        }
+    except Exception as exp:
+        print(exp)
+        return{
+            "statusCode": 500,
+            "body": json.dumps({"message": str(exp)})
+        }
+    
